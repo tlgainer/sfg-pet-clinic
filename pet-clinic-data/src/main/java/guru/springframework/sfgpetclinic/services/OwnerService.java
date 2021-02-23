e5d9f7245b700c11741de154c39939fba9616765
tlgainer <tlcheaves@outlook.com>
2021-02-23 14:17:30 -0500

    Adding vet and owner service interface.  #5

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findById(Long id);
+    Owner save(Owner pet);
+    Set<Owner> findAll();
+}
