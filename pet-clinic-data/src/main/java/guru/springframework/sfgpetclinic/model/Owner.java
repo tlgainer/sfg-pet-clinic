e881ae6e8dcc682f91675fade479f44300174486
tlgainer <tlcheaves@outlook.com>
2021-02-25 11:21:23 -0500

    add contact info to owner. closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -4,13 +4,40 @@ import java.util.Set;
 
 public class Owner extends Person {
 
-    private Set<Pet> pet;
+    private Set<Pet> pets;
+    private String address;
+    private String telephone;
+    private String city;
 
-    public Set<Pet> getPet() {
-        return pet;
+    public Set<Pet> getPets() {
+        return pets;
     }
 
-    public void setPet(Set<Pet> pet) {
-        this.pet = pet;
+    public void setPets(Set<Pet> pet) {
+        this.pets = pet;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
     }
 }
